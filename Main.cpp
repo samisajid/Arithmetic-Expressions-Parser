183f5b42faa5e846b44a3403c697b37d1d1f1a51
wydadman <42750788+wydadman@users.noreply.github.com>
2019-01-09 18:41:59 +0000

    Reporting more errors
    New errors reported:
    - The presence of two consecutive operators like '*+' or '/*' in the expression.
    - Raise a warning if there is not an equal number of left and right brackets in the expression

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int expression();
-const char toParse[] = "3+(5+(4*3))-12";
+char toParse[] = "3+(5+ (4-3 )-12";
 const char* expr = toParse;
 bool error = false;
 
@@ -18,14 +18,12 @@ char get()
 	return *expr++;
 }
 
-
-
 int number()
 {
-	int result = get()-'0';
+	int result = get() - '0';
 	while (peek() >= '0' && peek() <= '9')
 	{
-		result = 10 * result + get()-'0';
+		result = 10 * result + get() - '0';
 	}
 	return result;
 }
@@ -57,7 +55,7 @@ int mult()
 		if (get() == '*') {
 			result *= factor();
 		}
-			
+
 		else {
 			tmp = factor();
 			if (tmp == 0) {
@@ -90,26 +88,43 @@ int expression()
 
 int main()
 {
+	string operators = "+-/*";
 	string allowed = "0123456789 /+*-()";
 	int i;
-	for (i = 0; i < strlen(expr); i++) {
-		if (expr[i] == '/') {
-			if (expr[i + 1] == '0') {
-				cout << "You cannot divide by zero, try again";
-				return 0;
-			}
-		}
-		if (allowed.find(expr[i]) == string::npos) {
+	int k=0;
+	for (i = 0; i < strlen(toParse); i++) {
+		if (toParse[i] != ' ') {
+			toParse[k] = toParse[i];
+			k++;
+		}		
+	}
+	toParse[k] = '\0';
+
+	cout << k << endl;
+	cout << toParse << endl;
+	for (i = 0; i < strlen(toParse); i++) {
+		if (allowed.find(toParse[i]) == string::npos) {
 			//this checks if the element i is in the allowed list.
-			cout << "a '"<<expr[i]<<"' is trailing, get rid of it and try again";
+			cout << "A '" << toParse[i] << "' is trailing, get rid of it and try again";
 			//this is returned when the loop finds the first not allowed element.
 			return 0;
 		}
+		if (operators.find(toParse[i]) != string::npos){
+			if (operators.find(toParse[i + 1]) != string::npos){
+			cout << "You cannot use two operatros " << toParse[i] <<toParse[i+1] << " consecutively.";
+			return 0;
+			}
+		}
 	}
-	
+	if (count(toParse, toParse + k, '(') > count(toParse, toParse + k, ')')) {
+		cout << "Your result might be wrong as there are more '(' than ')'." << endl;
+	}
+	else if (count(toParse, toParse + k, '(') < count(toParse, toParse + k, ')')) {
+		cout << "Your result might be wrong as there are more ')' than '('." << endl;
+	}
 	int result = expression();
 	if (error == false) {
-		cout << "Your expression '"<< toParse <<"' equals: "<<result << endl;
+		cout << "Your expression '"<< toParse <<"' equals: "<<result <<"."<< endl;
 	}
 	
 	return 0;
